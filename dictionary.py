c39de979e08d9384569b937f99c2820b8831c370
Harshavardhana <harsha4688v@gmail.com>
2023-09-25 16:18:33 +0530

    learning dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+book = {
+    "gift": "this is a book of get gifts",
+    "this": "A key word in c++",
+    "instagram" : "where we post our pics",
+    "youtube" : "watch python videos"
+}
+
+print (book)
+print (book["this"])
+print (book["instagram"])
